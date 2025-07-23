fd5bffd8350cb30f40111c7c43a1f1dcae2c280d
Khorsed Alam <khorsedalam0472@gmail.com>
2025-07-24 01:43:43 +0600

    Var and Dynamic

--- bin/BasicDart/Variable.dart
+++ bin/BasicDart/Variable.dart
@@ -1,4 +1,25 @@
 void main(){
   String MyName='Khorsed Alam';
-  print(MyName);
+   int age=21;
+   double pi=3.1416;
+   bool isLogin=true;
+
+   MyName='Rohim';
+   print("Name: "+MyName);
+   print("Name: $MyName Age: $age");
+
+   var test='ok';
+   print("Test: $test");
+  //test=10; var is not dynamic thats why we can not change the value of var
+   print("Test: $test");
+
+   dynamic test1='Khorsed';// we can any type of data into the dynamic variable
+   print(test1);
+   test1=10;
+   print("print after using dynamic : $test1");
+   test1=true;
+   print("Print after using Dynamic: $test1");
+
+
+
 }
