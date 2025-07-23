4157c424b117a88e043f1f98fa83473d2487809e
Khorsed Alam <khorsedalam0472@gmail.com>
2025-07-24 02:58:02 +0600

    Dart Operation

---
+++ bin/BasicDart/DartOperation.dart
@@ -0,0 +1,29 @@
+void main(){
+   var A=10;
+   var B=20;
+   //Arithmetic Operation
+  print("Addition Operation  ${A+B}" );
+  print("Subtruction Operation  ${A-B}" );
+  print("Multiplication  Operation  ${A*B}" );
+  print("Division Operation  ${A/B}" );
+  print("Modulus Operation  ${A%B}\n\n" );
+
+
+  print("Pre and Post Increment : \n");
+  // Pre Increment
+  print(++A);
+
+  //Post Increment
+  print(B++);
+  print(B);
+
+  print("Pre and post decrement : \n");
+
+  //pre decrement
+  print(--A);
+
+  //Post Decrement
+  print(B--);
+  print(B);
+
+}
