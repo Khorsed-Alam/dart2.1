b904f3718349f2d6b1edb5bd597a1716f7690972
Khorsed Alam <khorsedalam0472@gmail.com>
2025-07-24 22:53:40 +0600

    Null safty

---
+++ bin/BasicDart/Null Safety  and  try catch.dart
@@ -0,0 +1,24 @@
+void main(){
+
+  int age=25;
+  int ? age2;
+  String ? Name;
+  print(age);
+  print(age2);
+  print(Name);
+ // Name='Khorsed';
+  print(Name);
+age>=20? print('Able to Vote'): print('Still kid');// ? single question mark represent tarrnery operator
+  print('Welcome to X ${Name ?? 'Guest'}');// ?? check Null and show defult value
+
+  // using late
+
+  late String Name2;// late use for create space but not use but when we use it will be assign by value
+  Name2='X';
+  print(Name2);
+
+
+
+
+
+}
